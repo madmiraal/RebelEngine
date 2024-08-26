1721c6a8734d962ce7ef89ad555f5360f07014a9
Marcel Admiraal <madmiraal@users.noreply.github.com>
2024-08-26 10:05:34 +0300

    Apply Rebel branding to core

--- core/type_info.h
+++ core/type_info.h
@@ -326,7 +326,7 @@ struct TypeInherits {
         && !TypesAreSame<B volatile const, void volatile const>::value;
 };
 
-namespace GodotTypeInfo
+namespace RebelTypeInfo
 {
 enum Metadata {
     METADATA_NONE,
@@ -341,7 +341,7 @@ enum Metadata {
     METADATA_REAL_IS_FLOAT,
     METADATA_REAL_IS_DOUBLE
 };
-} // namespace GodotTypeInfo
+} // namespace RebelTypeInfo
 
 // If the compiler fails because it's trying to instantiate the primary
 // 'GetTypeInfo' template instead of one of the specializations, it's most
@@ -357,8 +357,8 @@ struct GetTypeInfo;
     template <>                                                                \
     struct GetTypeInfo<m_type> {                                               \
         static const Variant::Type VARIANT_TYPE = m_var_type;                  \
-        static const GodotTypeInfo::Metadata METADATA =                        \
-            GodotTypeInfo::METADATA_NONE;                                      \
+        static const RebelTypeInfo::Metadata METADATA =                        \
+            RebelTypeInfo::METADATA_NONE;                                      \
         static inline PropertyInfo get_class_info() {                          \
             return PropertyInfo(VARIANT_TYPE, String());                       \
         }                                                                      \
@@ -366,8 +366,8 @@ struct GetTypeInfo;
     template <>                                                                \
     struct GetTypeInfo<const m_type&> {                                        \
         static const Variant::Type VARIANT_TYPE = m_var_type;                  \
-        static const GodotTypeInfo::Metadata METADATA =                        \
-            GodotTypeInfo::METADATA_NONE;                                      \
+        static const RebelTypeInfo::Metadata METADATA =                        \
+            RebelTypeInfo::METADATA_NONE;                                      \
         static inline PropertyInfo get_class_info() {                          \
             return PropertyInfo(VARIANT_TYPE, String());                       \
         }                                                                      \
@@ -377,7 +377,7 @@ struct GetTypeInfo;
     template <>                                                                \
     struct GetTypeInfo<m_type> {                                               \
         static const Variant::Type VARIANT_TYPE       = m_var_type;            \
-        static const GodotTypeInfo::Metadata METADATA = m_metadata;            \
+        static const RebelTypeInfo::Metadata METADATA = m_metadata;            \
         static inline PropertyInfo get_class_info() {                          \
             return PropertyInfo(VARIANT_TYPE, String());                       \
         }                                                                      \
@@ -385,7 +385,7 @@ struct GetTypeInfo;
     template <>                                                                \
     struct GetTypeInfo<const m_type&> {                                        \
         static const Variant::Type VARIANT_TYPE       = m_var_type;            \
-        static const GodotTypeInfo::Metadata METADATA = m_metadata;            \
+        static const RebelTypeInfo::Metadata METADATA = m_metadata;            \
         static inline PropertyInfo get_class_info() {                          \
             return PropertyInfo(VARIANT_TYPE, String());                       \
         }                                                                      \
@@ -395,53 +395,53 @@ MAKE_TYPE_INFO(bool, Variant::BOOL)
 MAKE_TYPE_INFO_WITH_META(
     uint8_t,
     Variant::INT,
-    GodotTypeInfo::METADATA_INT_IS_UINT8
+    RebelTypeInfo::METADATA_INT_IS_UINT8
 )
 MAKE_TYPE_INFO_WITH_META(
     int8_t,
     Variant::INT,
-    GodotTypeInfo::METADATA_INT_IS_INT8
+    RebelTypeInfo::METADATA_INT_IS_INT8
 )
 MAKE_TYPE_INFO_WITH_META(
     uint16_t,
     Variant::INT,
-    GodotTypeInfo::METADATA_INT_IS_UINT16
+    RebelTypeInfo::METADATA_INT_IS_UINT16
 )
 MAKE_TYPE_INFO_WITH_META(
     int16_t,
     Variant::INT,
-    GodotTypeInfo::METADATA_INT_IS_INT16
+    RebelTypeInfo::METADATA_INT_IS_INT16
 )
 MAKE_TYPE_INFO_WITH_META(
     uint32_t,
     Variant::INT,
-    GodotTypeInfo::METADATA_INT_IS_UINT32
+    RebelTypeInfo::METADATA_INT_IS_UINT32
 )
 MAKE_TYPE_INFO_WITH_META(
     int32_t,
     Variant::INT,
-    GodotTypeInfo::METADATA_INT_IS_INT32
+    RebelTypeInfo::METADATA_INT_IS_INT32
 )
 MAKE_TYPE_INFO_WITH_META(
     uint64_t,
     Variant::INT,
-    GodotTypeInfo::METADATA_INT_IS_UINT64
+    RebelTypeInfo::METADATA_INT_IS_UINT64
 )
 MAKE_TYPE_INFO_WITH_META(
     int64_t,
     Variant::INT,
-    GodotTypeInfo::METADATA_INT_IS_INT64
+    RebelTypeInfo::METADATA_INT_IS_INT64
 )
 MAKE_TYPE_INFO(wchar_t, Variant::INT)
 MAKE_TYPE_INFO_WITH_META(
     float,
     Variant::REAL,
-    GodotTypeInfo::METADATA_REAL_IS_FLOAT
+    RebelTypeInfo::METADATA_REAL_IS_FLOAT
 )
 MAKE_TYPE_INFO_WITH_META(
     double,
     Variant::REAL,
-    GodotTypeInfo::METADATA_REAL_IS_DOUBLE
+    RebelTypeInfo::METADATA_REAL_IS_DOUBLE
 )
 
 MAKE_TYPE_INFO(String, Variant::STRING)
@@ -477,8 +477,8 @@ MAKE_TYPE_INFO(BSP_Tree, Variant::DICTIONARY)
 template <>
 struct GetTypeInfo<RefPtr> {
     static const Variant::Type VARIANT_TYPE = Variant::OBJECT;
-    static const GodotTypeInfo::Metadata METADATA =
-        GodotTypeInfo::METADATA_NONE;
+    static const RebelTypeInfo::Metadata METADATA =
+        RebelTypeInfo::METADATA_NONE;
 
     static inline PropertyInfo get_class_info() {
         return PropertyInfo(
@@ -493,8 +493,8 @@ struct GetTypeInfo<RefPtr> {
 template <>
 struct GetTypeInfo<const RefPtr&> {
     static const Variant::Type VARIANT_TYPE = Variant::OBJECT;
-    static const GodotTypeInfo::Metadata METADATA =
-        GodotTypeInfo::METADATA_NONE;
+    static const RebelTypeInfo::Metadata METADATA =
+        RebelTypeInfo::METADATA_NONE;
 
     static inline PropertyInfo get_class_info() {
         return PropertyInfo(
@@ -510,8 +510,8 @@ struct GetTypeInfo<const RefPtr&> {
 template <>
 struct GetTypeInfo<Variant> {
     static const Variant::Type VARIANT_TYPE = Variant::NIL;
-    static const GodotTypeInfo::Metadata METADATA =
-        GodotTypeInfo::METADATA_NONE;
+    static const RebelTypeInfo::Metadata METADATA =
+        RebelTypeInfo::METADATA_NONE;
 
     static inline PropertyInfo get_class_info() {
         return PropertyInfo(
@@ -527,8 +527,8 @@ struct GetTypeInfo<Variant> {
 template <>
 struct GetTypeInfo<const Variant&> {
     static const Variant::Type VARIANT_TYPE = Variant::NIL;
-    static const GodotTypeInfo::Metadata METADATA =
-        GodotTypeInfo::METADATA_NONE;
+    static const RebelTypeInfo::Metadata METADATA =
+        RebelTypeInfo::METADATA_NONE;
 
     static inline PropertyInfo get_class_info() {
         return PropertyInfo(
@@ -545,8 +545,8 @@ struct GetTypeInfo<const Variant&> {
     template <>                                                                \
     struct GetTypeInfo<m_template<m_type>> {                                   \
         static const Variant::Type VARIANT_TYPE = m_var_type;                  \
-        static const GodotTypeInfo::Metadata METADATA =                        \
-            GodotTypeInfo::METADATA_NONE;                                      \
+        static const RebelTypeInfo::Metadata METADATA =                        \
+            RebelTypeInfo::METADATA_NONE;                                      \
         static inline PropertyInfo get_class_info() {                          \
             return PropertyInfo(VARIANT_TYPE, String());                       \
         }                                                                      \
@@ -554,8 +554,8 @@ struct GetTypeInfo<const Variant&> {
     template <>                                                                \
     struct GetTypeInfo<const m_template<m_type>&> {                            \
         static const Variant::Type VARIANT_TYPE = m_var_type;                  \
-        static const GodotTypeInfo::Metadata METADATA =                        \
-            GodotTypeInfo::METADATA_NONE;                                      \
+        static const RebelTypeInfo::Metadata METADATA =                        \
+            RebelTypeInfo::METADATA_NONE;                                      \
         static inline PropertyInfo get_class_info() {                          \
             return PropertyInfo(VARIANT_TYPE, String());                       \
         }                                                                      \
@@ -582,8 +582,8 @@ struct GetTypeInfo<
     T*,
     typename EnableIf<TypeInherits<Object, T>::value>::type> {
     static const Variant::Type VARIANT_TYPE = Variant::OBJECT;
-    static const GodotTypeInfo::Metadata METADATA =
-        GodotTypeInfo::METADATA_NONE;
+    static const RebelTypeInfo::Metadata METADATA =
+        RebelTypeInfo::METADATA_NONE;
 
     static inline PropertyInfo get_class_info() {
         return PropertyInfo(StringName(T::get_class_static()));
@@ -595,8 +595,8 @@ struct GetTypeInfo<
     const T*,
     typename EnableIf<TypeInherits<Object, T>::value>::type> {
     static const Variant::Type VARIANT_TYPE = Variant::OBJECT;
-    static const GodotTypeInfo::Metadata METADATA =
-        GodotTypeInfo::METADATA_NONE;
+    static const RebelTypeInfo::Metadata METADATA =
+        RebelTypeInfo::METADATA_NONE;
 
     static inline PropertyInfo get_class_info() {
         return PropertyInfo(StringName(T::get_class_static()));
@@ -607,8 +607,8 @@ struct GetTypeInfo<
     template <>                                                                \
     struct GetTypeInfo<m_impl> {                                               \
         static const Variant::Type VARIANT_TYPE = Variant::INT;                \
-        static const GodotTypeInfo::Metadata METADATA =                        \
-            GodotTypeInfo::METADATA_NONE;                                      \
+        static const RebelTypeInfo::Metadata METADATA =                        \
+            RebelTypeInfo::METADATA_NONE;                                      \
         static inline PropertyInfo get_class_info() {                          \
             return PropertyInfo(                                               \
                 Variant::INT,                                                  \
