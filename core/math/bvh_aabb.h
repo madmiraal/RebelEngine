08543ce84670dcff6c026614d121b44f5284f67c
Marcel Admiraal <madmiraal@users.noreply.github.com>
2025-04-19 14:33:47 +0200

    Separate BVH declarations and definitions

--- core/math/bvh_aabb.h
+++ core/math/bvh_aabb.h
@@ -38,241 +38,318 @@ struct AABB {
     Point min;
     Point neg_max;
 
-    bool operator==(const AABB& o) const {
-        return (min == o.min) && (neg_max == o.neg_max);
-    }
-
-    bool operator!=(const AABB& o) const {
-        return (*this == o) == false;
-    }
-
-    void set(const Point& _min, const Point& _max) {
-        min     = _min;
-        neg_max = -_max;
-    }
-
+    bool operator==(const AABB& o) const;
+    bool operator!=(const AABB& o) const;
+    void set(const Point& _min, const Point& _max);
     // To and from standard AABB.
-    void from(const BoundingBox& p_aabb) {
-        min     = p_aabb.position;
-        neg_max = -(p_aabb.position + p_aabb.size);
-    }
-
-    void to(BoundingBox& r_aabb) const {
-        r_aabb.position = min;
-        r_aabb.size     = calculate_size();
-    }
-
-    void merge(const AABB& p_o) {
-        for (int axis = 0; axis < Point::AXIS_COUNT; ++axis) {
-            neg_max[axis] = MIN(neg_max[axis], p_o.neg_max[axis]);
-            min[axis]     = MIN(min[axis], p_o.min[axis]);
-        }
-    }
-
-    Point calculate_size() const {
-        return -neg_max - min;
-    }
-
-    Point calculate_centre() const {
-        return Point((calculate_size() * 0.5) + min);
-    }
-
-    real_t get_proximity_to(const AABB& p_b) const {
-        const Point d    = (min - neg_max) - (p_b.min - p_b.neg_max);
-        real_t proximity = 0.0;
-        for (int axis = 0; axis < Point::AXIS_COUNT; ++axis) {
-            proximity += Math::abs(d[axis]);
-        }
-        return proximity;
-    }
-
-    int select_by_proximity(const AABB& p_a, const AABB& p_b) const {
-        return (get_proximity_to(p_a) < get_proximity_to(p_b) ? 0 : 1);
-    }
-
+    void from(const BoundingBox& p_aabb);
+    void to(BoundingBox& r_aabb) const;
+    void merge(const AABB& p_o);
+    Point calculate_size() const;
+    Point calculate_centre() const;
+    real_t get_proximity_to(const AABB& p_b) const;
+    int select_by_proximity(const AABB& p_a, const AABB& p_b) const;
     uint32_t find_cutting_planes(
         const typename AABB::ConvexHull& p_hull,
         uint32_t* p_plane_ids
-    ) const {
-        uint32_t count = 0;
-
-        for (int n = 0; n < p_hull.num_planes; n++) {
-            const Plane& p = p_hull.planes[n];
-            if (intersects_plane(p)) {
-                p_plane_ids[count++] = n;
-            }
-        }
-
-        return count;
-    }
-
-    bool intersects_plane(const Plane& p_p) const {
-        Vector3 size         = calculate_size();
-        Vector3 half_extents = size * 0.5;
-        Vector3 ofs          = min + half_extents;
-
-        // Forward side of plane.
-        Vector3 point_offset(
-            (p_p.normal.x < 0) ? -half_extents.x : half_extents.x,
-            (p_p.normal.y < 0) ? -half_extents.y : half_extents.y,
-            (p_p.normal.z < 0) ? -half_extents.z : half_extents.z
-        );
-        Vector3 point = point_offset + ofs;
-
-        if (!p_p.is_point_over(point)) {
-            return false;
-        }
-
-        point = -point_offset + ofs;
-        if (p_p.is_point_over(point)) {
-            return false;
-        }
-
-        return true;
-    }
-
+    ) const;
+    bool intersects_plane(const Plane& p_p) const;
     bool intersects_convex_optimized(
         const ConvexHull& p_hull,
         const uint32_t* p_plane_ids,
         uint32_t p_num_planes
-    ) const {
-        Vector3 size         = calculate_size();
-        Vector3 half_extents = size * 0.5;
-        Vector3 ofs          = min + half_extents;
-
-        for (unsigned int i = 0; i < p_num_planes; i++) {
-            const Plane& p = p_hull.planes[p_plane_ids[i]];
-            Vector3 point(
-                (p.normal.x > 0) ? -half_extents.x : half_extents.x,
-                (p.normal.y > 0) ? -half_extents.y : half_extents.y,
-                (p.normal.z > 0) ? -half_extents.z : half_extents.z
-            );
-            point += ofs;
-            if (p.is_point_over(point)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    bool intersects_convex_partial(const ConvexHull& p_hull) const {
-        BoundingBox bb;
-        to(bb);
-        return bb.intersects_convex_shape(
-            p_hull.planes,
-            p_hull.num_planes,
-            p_hull.points,
-            p_hull.num_points
-        );
-    }
-
-    IntersectResult intersects_convex(const ConvexHull& p_hull) const {
-        if (intersects_convex_partial(p_hull)) {
-            // Fully within is very important for tree checks.
-            if (is_within_convex(p_hull)) {
-                return IntersectResult::FULL;
-            }
-
-            return IntersectResult::PARTIAL;
-        }
-
-        return IntersectResult::MISS;
-    }
-
-    bool is_within_convex(const ConvexHull& p_hull) const {
-        // Use half extents routine.
-        BoundingBox bb;
-        to(bb);
-        return bb.inside_convex_shape(p_hull.planes, p_hull.num_planes);
-    }
-
+    ) const;
+    bool intersects_convex_partial(const ConvexHull& p_hull) const;
+    IntersectResult intersects_convex(const ConvexHull& p_hull) const;
+    bool is_within_convex(const ConvexHull& p_hull) const;
     bool is_point_within_hull(const ConvexHull& p_hull, const Vector3& p_pt)
-        const {
-        for (int n = 0; n < p_hull.num_planes; n++) {
-            if (p_hull.planes[n].distance_to(p_pt) > 0.0f) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    bool intersects_segment(const Segment& p_s) const {
-        BoundingBox bb;
-        to(bb);
-        return bb.intersects_segment(p_s.from, p_s.to);
-    }
-
-    bool intersects_point(const Point& p_pt) const {
-        if (_any_lessthan(-p_pt, neg_max)) {
-            return false;
-        }
-        if (_any_lessthan(p_pt, min)) {
-            return false;
-        }
-        return true;
-    }
-
-    bool intersects(const AABB& p_o) const {
-        if (_any_morethan(p_o.min, -neg_max)) {
-            return false;
-        }
-        if (_any_morethan(min, -p_o.neg_max)) {
-            return false;
-        }
-        return true;
-    }
-
-    bool is_other_within(const AABB& p_o) const {
-        if (_any_lessthan(p_o.neg_max, neg_max)) {
-            return false;
-        }
-        if (_any_lessthan(p_o.min, min)) {
-            return false;
-        }
-        return true;
-    }
-
-    void grow(const Point& p_change) {
-        neg_max -= p_change;
-        min     -= p_change;
-    }
-
-    void expand(real_t p_change) {
-        Point change;
-        change.set_all(p_change);
-        grow(change);
-    }
-
+        const;
+    bool intersects_segment(const Segment& p_s) const;
+    bool intersects_point(const Point& p_pt) const;
+    bool intersects(const AABB& p_o) const;
+    bool is_other_within(const AABB& p_o) const;
+    void grow(const Point& p_change);
+    void expand(real_t p_change);
     // Surface area metric.
-    float get_area() const {
-        Point d = calculate_size();
-        return 2.0f * (d.x * d.y + d.y * d.z + d.z * d.x);
-    }
-
-    void set_to_max_opposite_extents() {
-        neg_max.set_all(FLT_MAX);
-        min = neg_max;
-    }
-
-    bool _any_morethan(const Point& p_a, const Point& p_b) const {
-        for (int axis = 0; axis < Point::AXIS_COUNT; ++axis) {
-            if (p_a[axis] > p_b[axis]) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool _any_lessthan(const Point& p_a, const Point& p_b) const {
-        for (int axis = 0; axis < Point::AXIS_COUNT; ++axis) {
-            if (p_a[axis] < p_b[axis]) {
-                return true;
-            }
-        }
-        return false;
-    }
+    float get_area() const;
+    void set_to_max_opposite_extents();
+    bool _any_morethan(const Point& p_a, const Point& p_b) const;
+    bool _any_lessthan(const Point& p_a, const Point& p_b) const;
 };
+
+// Definitions
+
+template <typename BoundingBox, typename Point>
+bool AABB<BoundingBox, Point>::operator==(const AABB& o) const {
+    return (min == o.min) && (neg_max == o.neg_max);
+}
+
+template <typename BoundingBox, typename Point>
+bool AABB<BoundingBox, Point>::operator!=(const AABB& o) const {
+    return (*this == o) == false;
+}
+
+template <typename BoundingBox, typename Point>
+void AABB<BoundingBox, Point>::set(const Point& _min, const Point& _max) {
+    min     = _min;
+    neg_max = -_max;
+}
+
+template <typename BoundingBox, typename Point>
+void AABB<BoundingBox, Point>::from(const BoundingBox& p_aabb) {
+    min     = p_aabb.position;
+    neg_max = -(p_aabb.position + p_aabb.size);
+}
+
+template <typename BoundingBox, typename Point>
+void AABB<BoundingBox, Point>::to(BoundingBox& r_aabb) const {
+    r_aabb.position = min;
+    r_aabb.size     = calculate_size();
+}
+
+template <typename BoundingBox, typename Point>
+void AABB<BoundingBox, Point>::merge(const AABB& p_o) {
+    for (int axis = 0; axis < Point::AXIS_COUNT; ++axis) {
+        neg_max[axis] = MIN(neg_max[axis], p_o.neg_max[axis]);
+        min[axis]     = MIN(min[axis], p_o.min[axis]);
+    }
+}
+
+template <typename BoundingBox, typename Point>
+Point AABB<BoundingBox, Point>::calculate_size() const {
+    return -neg_max - min;
+}
+
+template <typename BoundingBox, typename Point>
+Point AABB<BoundingBox, Point>::calculate_centre() const {
+    return Point((calculate_size() * 0.5) + min);
+}
+
+template <typename BoundingBox, typename Point>
+real_t AABB<BoundingBox, Point>::get_proximity_to(const AABB& p_b) const {
+    const Point d    = (min - neg_max) - (p_b.min - p_b.neg_max);
+    real_t proximity = 0.0;
+    for (int axis = 0; axis < Point::AXIS_COUNT; ++axis) {
+        proximity += Math::abs(d[axis]);
+    }
+    return proximity;
+}
+
+template <typename BoundingBox, typename Point>
+int AABB<BoundingBox, Point>::select_by_proximity(
+    const AABB& p_a,
+    const AABB& p_b
+) const {
+    return (get_proximity_to(p_a) < get_proximity_to(p_b) ? 0 : 1);
+}
+
+template <typename BoundingBox, typename Point>
+uint32_t AABB<BoundingBox, Point>::find_cutting_planes(
+    const typename AABB::ConvexHull& p_hull,
+    uint32_t* p_plane_ids
+) const {
+    uint32_t count = 0;
+
+    for (int n = 0; n < p_hull.num_planes; n++) {
+        const Plane& p = p_hull.planes[n];
+        if (intersects_plane(p)) {
+            p_plane_ids[count++] = n;
+        }
+    }
+
+    return count;
+}
+
+template <typename BoundingBox, typename Point>
+bool AABB<BoundingBox, Point>::intersects_plane(const Plane& p_p) const {
+    Vector3 size         = calculate_size();
+    Vector3 half_extents = size * 0.5;
+    Vector3 ofs          = min + half_extents;
+
+    // Forward side of plane.
+    Vector3 point_offset(
+        (p_p.normal.x < 0) ? -half_extents.x : half_extents.x,
+        (p_p.normal.y < 0) ? -half_extents.y : half_extents.y,
+        (p_p.normal.z < 0) ? -half_extents.z : half_extents.z
+    );
+    Vector3 point = point_offset + ofs;
+
+    if (!p_p.is_point_over(point)) {
+        return false;
+    }
+
+    point = -point_offset + ofs;
+    if (p_p.is_point_over(point)) {
+        return false;
+    }
+
+    return true;
+}
+
+template <typename BoundingBox, typename Point>
+bool AABB<BoundingBox, Point>::intersects_convex_optimized(
+    const ConvexHull& p_hull,
+    const uint32_t* p_plane_ids,
+    uint32_t p_num_planes
+) const {
+    Vector3 size         = calculate_size();
+    Vector3 half_extents = size * 0.5;
+    Vector3 ofs          = min + half_extents;
+
+    for (unsigned int i = 0; i < p_num_planes; i++) {
+        const Plane& p = p_hull.planes[p_plane_ids[i]];
+        Vector3 point(
+            (p.normal.x > 0) ? -half_extents.x : half_extents.x,
+            (p.normal.y > 0) ? -half_extents.y : half_extents.y,
+            (p.normal.z > 0) ? -half_extents.z : half_extents.z
+        );
+        point += ofs;
+        if (p.is_point_over(point)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+template <typename BoundingBox, typename Point>
+bool AABB<BoundingBox, Point>::intersects_convex_partial(
+    const ConvexHull& p_hull
+) const {
+    BoundingBox bb;
+    to(bb);
+    return bb.intersects_convex_shape(
+        p_hull.planes,
+        p_hull.num_planes,
+        p_hull.points,
+        p_hull.num_points
+    );
+}
+
+template <typename BoundingBox, typename Point>
+IntersectResult AABB<BoundingBox, Point>::intersects_convex(
+    const ConvexHull& p_hull
+) const {
+    if (intersects_convex_partial(p_hull)) {
+        // Fully within is very important for tree checks.
+        if (is_within_convex(p_hull)) {
+            return IntersectResult::FULL;
+        }
+
+        return IntersectResult::PARTIAL;
+    }
+
+    return IntersectResult::MISS;
+}
+
+template <typename BoundingBox, typename Point>
+bool AABB<BoundingBox, Point>::is_within_convex(const ConvexHull& p_hull
+) const {
+    // Use half extents routine.
+    BoundingBox bb;
+    to(bb);
+    return bb.inside_convex_shape(p_hull.planes, p_hull.num_planes);
+}
+
+template <typename BoundingBox, typename Point>
+bool AABB<BoundingBox, Point>::is_point_within_hull(
+    const ConvexHull& p_hull,
+    const Vector3& p_pt
+) const {
+    for (int n = 0; n < p_hull.num_planes; n++) {
+        if (p_hull.planes[n].distance_to(p_pt) > 0.0f) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template <typename BoundingBox, typename Point>
+bool AABB<BoundingBox, Point>::intersects_segment(const Segment& p_s) const {
+    BoundingBox bb;
+    to(bb);
+    return bb.intersects_segment(p_s.from, p_s.to);
+}
+
+template <typename BoundingBox, typename Point>
+bool AABB<BoundingBox, Point>::intersects_point(const Point& p_pt) const {
+    if (_any_lessthan(-p_pt, neg_max)) {
+        return false;
+    }
+    if (_any_lessthan(p_pt, min)) {
+        return false;
+    }
+    return true;
+}
+
+template <typename BoundingBox, typename Point>
+bool AABB<BoundingBox, Point>::intersects(const AABB& p_o) const {
+    if (_any_morethan(p_o.min, -neg_max)) {
+        return false;
+    }
+    if (_any_morethan(min, -p_o.neg_max)) {
+        return false;
+    }
+    return true;
+}
+
+template <typename BoundingBox, typename Point>
+bool AABB<BoundingBox, Point>::is_other_within(const AABB& p_o) const {
+    if (_any_lessthan(p_o.neg_max, neg_max)) {
+        return false;
+    }
+    if (_any_lessthan(p_o.min, min)) {
+        return false;
+    }
+    return true;
+}
+
+template <typename BoundingBox, typename Point>
+void AABB<BoundingBox, Point>::grow(const Point& p_change) {
+    neg_max -= p_change;
+    min     -= p_change;
+}
+
+template <typename BoundingBox, typename Point>
+void AABB<BoundingBox, Point>::expand(real_t p_change) {
+    Point change;
+    change.set_all(p_change);
+    grow(change);
+}
+
+template <typename BoundingBox, typename Point>
+float AABB<BoundingBox, Point>::get_area() const {
+    Point d = calculate_size();
+    return 2.0f * (d.x * d.y + d.y * d.z + d.z * d.x);
+}
+
+template <typename BoundingBox, typename Point>
+void AABB<BoundingBox, Point>::set_to_max_opposite_extents() {
+    neg_max.set_all(FLT_MAX);
+    min = neg_max;
+}
+
+template <typename BoundingBox, typename Point>
+bool AABB<BoundingBox, Point>::_any_morethan(const Point& p_a, const Point& p_b)
+    const {
+    for (int axis = 0; axis < Point::AXIS_COUNT; ++axis) {
+        if (p_a[axis] > p_b[axis]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+template <typename BoundingBox, typename Point>
+bool AABB<BoundingBox, Point>::_any_lessthan(const Point& p_a, const Point& p_b)
+    const {
+    for (int axis = 0; axis < Point::AXIS_COUNT; ++axis) {
+        if (p_a[axis] < p_b[axis]) {
+            return true;
+        }
+    }
+    return false;
+}
 
 } // namespace BVH
 
