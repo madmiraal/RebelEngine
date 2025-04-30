d3b1d32fd43b1d7d5e1df9e452d310ea1d1f48ed
Marcel Admiraal <madmiraal@users.noreply.github.com>
2025-04-30 10:54:40 +0200

    Move BVH leaf and node into their own header files
    - Make BVH Leaf and Node classes
    - Separate BVH Leaf and Node definitions
    - Rename BVH TLeaf to Leaf and TNode to Node
    - Rename tnode variables to node
    
    Fix remove_item_unordered ERR

---
+++ core/math/bvh_leaf.h
@@ -0,0 +1,106 @@
+// SPDX-FileCopyrightText: 2023 Rebel Engine contributors
+// SPDX-FileCopyrightText: 2014-2022 Godot Engine contributors
+// SPDX-FileCopyrightText: 2007-2014 Juan Linietsky, Ariel Manzur
+//
+// SPDX-License-Identifier: MIT
+
+#ifndef BVH_LEAF_H
+#define BVH_LEAF_H
+
+namespace BVH {
+
+template <int MAX_ITEMS, class BoundingBox, class Point>
+class Leaf {
+public:
+    AABB<BoundingBox, Point>& get_aabb(uint32_t p_id);
+    const AABB<BoundingBox, Point>& get_aabb(uint32_t p_id) const;
+    uint32_t& get_item_ref_id(uint32_t p_id);
+    const uint32_t& get_item_ref_id(uint32_t p_id) const;
+    bool is_dirty() const;
+    void set_dirty(bool p);
+    void clear();
+    bool is_full() const;
+    void remove_item_unordered(uint32_t p_id);
+    uint32_t request_item();
+
+    uint16_t num_items;
+
+private:
+    uint16_t dirty;
+    // Separate data orientated lists for faster SIMD traversal.
+    uint32_t item_ref_ids[MAX_ITEMS];
+    AABB<BoundingBox, Point> aabbs[MAX_ITEMS];
+};
+
+// Definitions
+
+template <int MAX_ITEMS, class BoundingBox, class Point>
+AABB<BoundingBox, Point>& Leaf<MAX_ITEMS, BoundingBox, Point>::get_aabb(
+    uint32_t p_id
+) {
+    return aabbs[p_id];
+}
+
+template <int MAX_ITEMS, class BoundingBox, class Point>
+const AABB<BoundingBox, Point>& Leaf<MAX_ITEMS, BoundingBox, Point>::get_aabb(
+    uint32_t p_id
+) const {
+    return aabbs[p_id];
+}
+
+template <int MAX_ITEMS, class BoundingBox, class Point>
+uint32_t& Leaf<MAX_ITEMS, BoundingBox, Point>::get_item_ref_id(uint32_t p_id) {
+    return item_ref_ids[p_id];
+}
+
+template <int MAX_ITEMS, class BoundingBox, class Point>
+const uint32_t& Leaf<MAX_ITEMS, BoundingBox, Point>::get_item_ref_id(
+    uint32_t p_id
+) const {
+    return item_ref_ids[p_id];
+}
+
+template <int MAX_ITEMS, class BoundingBox, class Point>
+bool Leaf<MAX_ITEMS, BoundingBox, Point>::is_dirty() const {
+    return dirty;
+}
+
+template <int MAX_ITEMS, class BoundingBox, class Point>
+void Leaf<MAX_ITEMS, BoundingBox, Point>::set_dirty(bool p) {
+    dirty = p;
+}
+
+template <int MAX_ITEMS, class BoundingBox, class Point>
+void Leaf<MAX_ITEMS, BoundingBox, Point>::clear() {
+    num_items = 0;
+    set_dirty(true);
+}
+
+template <int MAX_ITEMS, class BoundingBox, class Point>
+bool Leaf<MAX_ITEMS, BoundingBox, Point>::is_full() const {
+    return num_items >= MAX_ITEMS;
+}
+
+template <int MAX_ITEMS, class BoundingBox, class Point>
+void Leaf<MAX_ITEMS, BoundingBox, Point>::remove_item_unordered(uint32_t p_id) {
+    ERR_FAIL_UNSIGNED_INDEX_MSG(
+        p_id,
+        num_items,
+        "Trying to remove an invalid BVH item"
+    );
+    num_items--;
+    aabbs[p_id]        = aabbs[num_items];
+    item_ref_ids[p_id] = item_ref_ids[num_items];
+}
+
+template <int MAX_ITEMS, class BoundingBox, class Point>
+uint32_t Leaf<MAX_ITEMS, BoundingBox, Point>::request_item() {
+    CRASH_COND_MSG(num_items >= MAX_ITEMS, "BVH leaf is full");
+    uint32_t id = num_items;
+    num_items++;
+    return id;
+}
+
+} // namespace BVH
+
+#endif // BVH_LEAF_H
