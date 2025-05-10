a96a14f1c0d61e03f086b83b79b387e5f0fdbc28
Marcel Admiraal <madmiraal@users.noreply.github.com>
2025-05-10 07:39:56 +0200

    Add convert physics engine to RebelPhysics

--- projects_manager/import_project.cpp
+++ projects_manager/import_project.cpp
@@ -4,6 +4,7 @@
 
 #include "import_project.h"
 
+#include "core/io/config_file.h"
 #include "core/os/dir_access.h"
 
 namespace {
@@ -46,6 +47,31 @@ bool create_rebel_project_from_godot_project(
     memdelete(dir_access);
     return true;
 }
+
+void rename_rebel_physics(const String& project_file) {
+    const String physics_section = "physics";
+    ConfigFile config_file(project_file);
+    if (!config_file.has_section(physics_section) {
+        return;
+    }
+
+    const String physics_2d_engine_key = "2d/physics_engine";
+    const String physics_3d_engine_key = "3d/physics_engine";
+    const String old_value = "GodotPhysics";
+    const String new_value = "RebelPhysics";
+    List<String> physics_keys;
+    config_file.get_section_keys(physics_section, physics_keys);
+    if (physics_keys.has(physics_2d_engine_key)
+        && config_file.get_value(physics_section, physics_2d_engine_key) == old_value) {
+        config_file.set_value(physics_section, physics_2d_engne_key, new_value);
+    }
+    if (physics_keys.has(physics_2d_engine_key)
+        && config_file.get_value(physics_section, physics_2d_engine_key) == old_value) {
+        config_file.set_value(physics_section, physics_2d_engne_key, new_value);
+    }
+
+    config_file.save();
+}
 } // namespace
 
 namespace ImportProject {
@@ -60,6 +86,8 @@ bool import_godot_project(
         ),
         false
     );
+    const String = project_file = destination_folder.plus_file("project.rebel");
+    rename_rebel_physics(project_file);
     return true;
 }
 } // namespace ImportProject
