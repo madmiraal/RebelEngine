337ba0bea42b0545a1025052b526f4cf0c583cbb
Marcel Admiraal <madmiraal@users.noreply.github.com>
2026-09-06 18:22:05 +0200

    Refactor DynamicFontData

--- scene/resources/fonts/dynamic_font_data.cpp
+++ scene/resources/fonts/dynamic_font_data.cpp
@@ -8,74 +8,84 @@
 
 #include "scene/resources/fonts/dynamic_font_at_size.h"
 
-bool DynamicFontData::CacheID::operator<(CacheID right) const {
+bool DynamicFontData::CacheID::operator<(const CacheID right) const {
     return key < right.key;
 }
 
-Ref<DynamicFontAtSize> DynamicFontData::_get_dynamic_font_at_size(
-    CacheID p_cache_id
-) {
-    if (size_cache.has(p_cache_id)) {
-        return Ref<DynamicFontAtSize>(size_cache[p_cache_id]);
-    }
-
-    Ref<DynamicFontAtSize> dfas;
-
-    dfas.instance();
-
-    dfas->font = Ref<DynamicFontData>(this);
-
-    size_cache[p_cache_id] = dfas.ptr();
-    dfas->id               = p_cache_id;
-    dfas->_load();
-
-    return dfas;
+bool DynamicFontData::is_antialiased() const {
+    return antialiased;
 }
 
-void DynamicFontData::set_font_ptr(
-    const uint8_t* p_font_mem,
-    int p_font_mem_size
-) {
-    font_mem      = p_font_mem;
-    font_mem_size = p_font_mem_size;
-}
-
-void DynamicFontData::set_font_path(const String& p_path) {
-    font_path = p_path;
+void DynamicFontData::set_antialiased(const bool new_antialiased) {
+    antialiased = new_antialiased;
 }
 
 String DynamicFontData::get_font_path() const {
     return font_path;
 }
 
-void DynamicFontData::set_force_autohinter(bool p_force) {
-    force_autohinter = p_force;
+void DynamicFontData::set_font_path(const String& new_font_path) {
+    font_path = new_font_path;
+}
+
+void DynamicFontData::set_font_bytes(
+    const unsigned char* new_font_bytes,
+    const int new_font_bytes_length
+) {
+    font_bytes        = new_font_bytes;
+    font_bytes_length = new_font_bytes_length;
+}
+
+DynamicFontData::Hinting DynamicFontData::get_hinting() const {
+    return hinting;
+}
+
+void DynamicFontData::set_hinting(const Hinting new_hinting) {
+    hinting = new_hinting;
+}
+
+void DynamicFontData::set_force_auto_hinter(const bool new_force_auto_hinting) {
+    force_auto_hinter = new_force_auto_hinting;
+}
+
+Ref<DynamicFontAtSize> DynamicFontData::get_font_at_size(const CacheID cache_id
+) {
+    if (font_at_sizes_cache.has(cache_id)) {
+        return {font_at_sizes_cache[cache_id]};
+    }
+    Ref<DynamicFontAtSize> font_at_size;
+    font_at_size.instance();
+    font_at_size->font            = Ref<DynamicFontData>(this);
+    font_at_sizes_cache[cache_id] = font_at_size.ptr();
+    font_at_size->id              = cache_id;
+    font_at_size->_load();
+    return font_at_size;
 }
 
 void DynamicFontData::_bind_methods() {
-    ClassDB::bind_method(
-        D_METHOD("set_antialiased", "antialiased"),
-        &DynamicFontData::set_antialiased
-    );
     ClassDB::bind_method(
         D_METHOD("is_antialiased"),
         &DynamicFontData::is_antialiased
     );
     ClassDB::bind_method(
-        D_METHOD("set_font_path", "path"),
-        &DynamicFontData::set_font_path
+        D_METHOD("set_antialiased", "antialiased"),
+        &DynamicFontData::set_antialiased
     );
     ClassDB::bind_method(
         D_METHOD("get_font_path"),
         &DynamicFontData::get_font_path
     );
     ClassDB::bind_method(
-        D_METHOD("set_hinting", "mode"),
-        &DynamicFontData::set_hinting
+        D_METHOD("set_font_path", "path"),
+        &DynamicFontData::set_font_path
     );
     ClassDB::bind_method(
         D_METHOD("get_hinting"),
         &DynamicFontData::get_hinting
+    );
+    ClassDB::bind_method(
+        D_METHOD("set_hinting", "mode"),
+        &DynamicFontData::set_hinting
     );
 
     ADD_PROPERTY(
@@ -98,8 +108,8 @@ void DynamicFontData::_bind_methods() {
     BIND_ENUM_CONSTANT(HINTING_LIGHT);
     BIND_ENUM_CONSTANT(HINTING_NORMAL);
 
-    // Only WOFF1 is supported as WOFF2 requires a Brotli decompression library
-    // to be linked.
+    // Only WOFF1 is supported.
+    // WOFF2 requires a Brotli decompression library.
     ADD_PROPERTY(
         PropertyInfo(
             Variant::STRING,
@@ -110,36 +120,4 @@ void DynamicFontData::_bind_methods() {
         "set_font_path",
         "get_font_path"
     );
-}
-
-DynamicFontData::DynamicFontData() {
-    antialiased      = true;
-    force_autohinter = false;
-    hinting          = DynamicFontData::HINTING_NORMAL;
-    font_mem         = nullptr;
-    font_mem_size    = 0;
-}
-
-DynamicFontData::~DynamicFontData() {}
-
-bool DynamicFontData::is_antialiased() const {
-    return antialiased;
-}
-
-void DynamicFontData::set_antialiased(bool p_antialiased) {
-    if (antialiased == p_antialiased) {
-        return;
-    }
-    antialiased = p_antialiased;
-}
-
-DynamicFontData::Hinting DynamicFontData::get_hinting() const {
-    return hinting;
-}
-
-void DynamicFontData::set_hinting(Hinting p_hinting) {
-    if (hinting == p_hinting) {
-        return;
-    }
-    hinting = p_hinting;
 }
