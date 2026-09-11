cac9c1c6e4eebf95b73c3f7739eabf78a9575d15
Marcel Admiraal <madmiraal@users.noreply.github.com>
2026-09-11 08:43:44 +0200

    Move FontData initialization to FontData

--- scene/resources/fonts/dynamic_font_at_size.h
+++ scene/resources/fonts/dynamic_font_at_size.h
@@ -17,8 +17,6 @@
 #include "scene/resources/fonts/dynamic_font_settings.h"
 #include "scene/resources/texture.h"
 
-#include <ft2build.h>
-#include FT_FREETYPE_H
 #include FT_GLYPH_H
 
 class ImageTexture;
@@ -47,6 +45,7 @@ public:
 
     static float font_oversampling;
 
+    DynamicFontAtSize() = default;
     ~DynamicFontAtSize() override;
 
     float get_ascent() const;
@@ -74,9 +73,7 @@ public:
 private:
     friend class DynamicFontData;
 
-    FT_Face ft_face        = nullptr;
-    FT_Library ft_library  = nullptr;
-    FT_StreamRec ft_stream = {};
+    FT_Face ft_face = nullptr;
 
     Ref<DynamicFontData> font_data;
     DynamicFontSettings font_settings;
