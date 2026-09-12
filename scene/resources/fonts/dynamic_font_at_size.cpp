67729ce480e0efb8c57a08c88d0814779587ea6a
Marcel Admiraal <madmiraal@users.noreply.github.com>
2026-09-12 10:13:01 +0200

    Fix FT_Face memory leak

--- scene/resources/fonts/dynamic_font_at_size.cpp
+++ scene/resources/fonts/dynamic_font_at_size.cpp
@@ -83,6 +83,9 @@ constexpr static float get_kerning_advance(
 }
 
 DynamicFontAtSize::~DynamicFontAtSize() {
+    if (ft_face) {
+        FT_Done_Face(ft_face);
+    }
     font_data->font_at_sizes_cache.erase(font_settings);
     font_data.unref();
 }
@@ -223,6 +226,9 @@ Error DynamicFontAtSize::load() {
     ft_open_args.flags        = FT_OPEN_MEMORY;
     ft_open_args.stream       = ft_stream;
 
+    if (ft_face) {
+        FT_Done_Face(ft_face);
+    }
     const FT_Error ft_error =
         FT_Open_Face(ft_library, &ft_open_args, 0, &ft_face);
     if (ft_error) {
