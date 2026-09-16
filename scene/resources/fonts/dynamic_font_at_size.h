be09f4b36598ecd53a96812e18352549267eb0cc
Marcel Admiraal <madmiraal@users.noreply.github.com>
2026-09-16 12:19:16 +0200

    Use Ref<DynamicFontAtSize> instead of pointer to DynamicFontAtSize

--- scene/resources/fonts/dynamic_font_at_size.h
+++ scene/resources/fonts/dynamic_font_at_size.h
@@ -88,7 +88,7 @@ private:
     bool valid               = false;
 
     Error load();
-    Pair<const CharacterData&, const DynamicFontAtSize*>
+    Pair<const CharacterData&, const Ref<DynamicFontAtSize>>
     get_character_data_and_font(
         CharType character,
         const Vector<Ref<DynamicFontAtSize>>& fallbacks
